b416c0a9ed6e91bcc0d30e4e24208554f1df4bca
Chinmayeep58 <chinmayee05rao@gmail.com>
2025-08-26 22:37:39 +0530

    26-8

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 df=pd.read_csv("salaries.csv")
 # print(df.head())
@@ -10,4 +12,26 @@ target=df['salary_more_than_100k']
 # print(inputs)
 # print(target)
 
+le_company=LabelEncoder()
+le_job=LabelEncoder()
+le_degree=LabelEncoder()
 
+inputs['company_n']=le_company.fit_transform(inputs['company'])
+inputs['job_n']=le_company.fit_transform(inputs['job'])
+inputs['degree_n']=le_company.fit_transform(inputs['degree'])
+
+# print(inputs)
+
+inputs_n=inputs.drop(['company','job','degree'],axis='columns')
+# print(inputs_n)
+
+model=tree.DecisionTreeClassifier()
+x_train,x_test,y_train,y_test=train_test_split(inputs_n,target,test_size=0.2)
+
+model.fit(x_train,y_train)
+
+sc=model.score(x_test,y_test)
+# print(sc)
+
+ans=model.predict([[2,2,1]])
+print(ans)
---
+++ support_vector_machine.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+iris=load_iris()
+df=pd.DataFrame(iris.data,columns=iris.feature_names)
+# print(df.head())
+df['target']=iris.target
+# print(df.head())
+
+# print(iris.target_names)
+
+df['flower_name']=df.target.apply(lambda x:iris.target_names[x])
+# print(df.head())
+
+df0=df[df.target==0]
+df1=df[df.target==1]
+df2=df[df.target==2]
+plt.scatter(df0['sepal length (cm)'],df0['sepal width (cm)'],color='green',marker='+')
+plt.scatter(df1['sepal length (cm)'],df1['sepal width (cm)'],color='blue',marker='.')
+plt.scatter(df2['sepal length (cm)'],df2['sepal width (cm)'],color='red',marker='*')
+# plt.show()
+
+x=df.drop(['target','flower_name'],axis='columns')
+y=df.target
+
+x_train, x_test, y_train, y_test= train_test_split(x,y,test_size=0.2)
+
+model=SVC(gamma=100)
+model.fit(x_train,y_train)
+print(model.score(x_train,y_train))
