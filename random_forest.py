162a95b4076cc14a706fbb25e76f2f48eed10176
Chinmayeep58 <chinmayee05rao@gmail.com>
2025-08-26 23:44:55 +0530

    26-8/2

---
+++ random_forest.py
@@ -0,0 +1,3 @@
+#we divide the dataset into random datasets
+#then make decision trees for each random dataset
+#since many trees are formed, its called a forest
--- svc_ex.py
+++ svc_ex.py
@@ -1,1 +1,32 @@
-import 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
+digits=load_digits()
+# print(dir(digits))
+df=pd.DataFrame(digits.data,digits.target)
+# print(df.head())
+df['target']=digits.target
+# print(df.head())
+
+# print(digits.target_names)
+
+# df['number']=df.target.apply(lambda x:digits.target_names[x])
+
+# print(df.head())
+
+
+x=df.drop(['target'],axis='columns')
+y=df.target
+
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2)
+
+rbf_model=SVC(kernel='rbf')
+rbf_model.fit(x_train,y_train)
+print(rbf_model.score(x_test,y_test))
+
+linear_model=SVC(kernel='linear')
+linear_model.fit(x_train,y_train)
+print(linear_model.score(x_test,y_test))
