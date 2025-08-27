7e8986988119dea468a62d491568dbf7e73af118
Chinmayeep58 <chinmayee05rao@gmail.com>
2025-08-27 13:29:09 +0530

    27-8

--- k_fold.py
+++ k_fold.py
@@ -18,16 +18,36 @@ x_train, x_test, y_train, y_test=train_test_split(digits.data, digits.target,tes
 
 lr=LogisticRegression()
 lr.fit(x_train,y_train)
-print(lr.score(x_test,y_test))
+# print(lr.score(x_test,y_test))
 
 svc=SVC()
 svc.fit(x_train,y_train)
-print(svc.score(x_test,y_test))
+# print(svc.score(x_test,y_test))
 
 rf=RandomForestClassifier()
 rf.fit(x_train,y_train)
-print(rf.score(x_test,y_test))
+# print(rf.score(x_test,y_test))
 
 kf=KFold(n_splits=3)
 for train_index, test_index in kf.split([1,2,3,4,5,6,7,8,9]):
     print(train_index,test_index)
+
+def get_score(model,x_train,x_test,y_train,y_test):
+    model.fit(x_train,y_train)
+    return model.score(x_test,y_test)
+
+# stratified k fold ensures the data division has uniformity
+scores_l=[]
+scores_svm=[]
+scores_rf=[]
+
+for train_index, test_index in kf.split(digits.data):
+    x_train, x_test,y_train,y_test=digits.data[train_index],digits.data[test_index],digits.target[train_index],digits.target[test_index]
+
+    scores_l.append(get_score(LogisticRegression(),x_train,x_test,y_train,y_test))
+    scores_svm.append(get_score(SVC(),x_train,x_test,y_train,y_test))
+    scores_rf.append(get_score(RandomForestClassifier(),x_train,x_test,y_train,y_test))
+    
+print(scores_l)
+print(scores_svm)
+print(scores_rf)
